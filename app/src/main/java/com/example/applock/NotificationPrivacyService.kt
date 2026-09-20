a48daab698b2dc5f4c4f2e30229c10715e815d75
aqiffarooqui-dot <aqiffarooqui@gmail.com>
2026-09-20 17:14:14 +0530

    Update NotificationPrivacyService.kt

--- app/src/main/java/com/example/applock/NotificationPrivacyService.kt
+++ app/src/main/java/com/example/applock/NotificationPrivacyService.kt
@@ -6,30 +6,123 @@ import android.util.Log
 
 class NotificationPrivacyService : NotificationListenerService() {
 
-    override fun onNotificationPosted(sbn: StatusBarNotification?) {
-        if (sbn == null) return
+    companion object {
+
+        private var serviceInstance: NotificationPrivacyService? = null
+
+        fun refreshLockedApps() {
+            serviceInstance?.refreshExistingNotifications()
+        }
+    }
+
+    override fun onListenerConnected() {
+        super.onListenerConnected()
+
+        serviceInstance = this
+
+        Log.d(
+            "NotificationPrivacy",
+            "Notification listener connected"
+        )
+
+        refreshExistingNotifications()
+    }
+
+    override fun onListenerDisconnected() {
+        serviceInstance = null
+        super.onListenerDisconnected()
+    }
+
+    override fun onNotificationPosted(
+        sbn: StatusBarNotification?
+    ) {
+
+        if (sbn == null) {
+            return
+        }
+
+        hideIfRequired(sbn)
+    }
+
+    private fun refreshExistingNotifications() {
 
         if (!SettingsManager.isNotificationPrivacyEnabled(this)) {
             return
         }
 
-        val packageName = sbn.packageName
+        try {
 
-        if (AppLockPreferences.isLocked(this, packageName)) {
-            try {
-                cancelNotification(sbn.key)
+            val activeNotifications =
+                activeNotifications ?: return
 
-                Log.d(
-                    "NotificationPrivacy",
-                    "Hidden notification from: $packageName"
-                )
-            } catch (e: Exception) {
-                Log.e(
-                    "NotificationPrivacy",
-                    "Unable to hide notification",
-                    e
-                )
+            for (notification in activeNotifications) {
+                hideIfRequired(notification)
             }
+
+        } catch (e: Exception) {
+
+            Log.e(
+                "NotificationPrivacy",
+                "Unable to refresh notifications",
+                e
+            )
         }
+    }
+
+    private fun hideIfRequired(
+        sbn: StatusBarNotification
+    ) {
+
+        if (
+            !SettingsManager
+                .isNotificationPrivacyEnabled(this)
+        ) {
+            return
+        }
+
+        val packageName =
+            sbn.packageName
+
+        // Never hide Farooqui App Lock's own notifications.
+        if (packageName == this.packageName) {
+            return
+        }
+
+        if (
+            !AppLockPreferences
+                .isLocked(
+                    this,
+                    packageName
+                )
+        ) {
+            return
+        }
+
+        try {
+
+            cancelNotification(sbn.key)
+
+            Log.d(
+                "NotificationPrivacy",
+                "Hidden notification from: $packageName"
+            )
+
+        } catch (e: Exception) {
+
+            Log.e(
+                "NotificationPrivacy",
+                "Unable to hide notification",
+                e
+            )
+        }
+    }
+
+    override fun onDestroy() {
+
+        if (serviceInstance === this) {
+            serviceInstance = null
+        }
+
+        super.onDestroy()
     }
 }
