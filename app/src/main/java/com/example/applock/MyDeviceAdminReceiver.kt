31fe770a1fc7b167d43997749e29e2a7b63289f2
aqiffarooqui-dot <aqiffarooqui@gmail.com>
2026-09-20 13:04:08 +0530

    Update MyDeviceAdminReceiver.kt

--- app/src/main/java/com/example/applock/MyDeviceAdminReceiver.kt
+++ app/src/main/java/com/example/applock/MyDeviceAdminReceiver.kt
@@ -1,9 +1,9 @@
 package com.example.applock
 
-app.admin.DeviceAdminReceiver
-content.Context
-content.Intent
-widget.Toast
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 
 class MyDeviceAdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context, intent: Intent) {
